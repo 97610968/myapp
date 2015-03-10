0144c4092267a77eeaaa239a5ab316ee7fb607a4
97610968 <gossaca@gmail.com>
2015-03-10 15:19:19 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,48 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "myapp",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/97610968/myapp/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  547,
+  64,
+  652507,
+  145170,
+  277980,
+  383,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-03-10T07:04:29.170Z",
+  "2015-03-10T07:04:29.168Z",
+  "2015-03-10T07:04:29.228Z",
+  "2015-03-10T07:04:54.873Z",
+  "2015-03-10T07:04:29.191Z",
+  "2015-03-10T07:04:29.193Z",
+  "2015-03-10T07:04:29.162Z",
+  "2015-03-10T07:04:29.144Z",
+  "2015-03-10T07:04:41.022Z"
+ ],
+ "date": "2015-03-10T07:04:55.843Z"
+})
